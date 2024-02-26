3e983cf4cfc89628aeab7d40c6805a26096605ab
Gabriel <you@example.comgabrielnino@edu.unifil.br>
2024-02-26 14:36:42 -0300

    Config: Criação do frontend

---
+++ client/src/pages/cadastro-usuario/Auth-User.js
@@ -0,0 +1,177 @@
+import React, { useState } from 'react';
+
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+export default function Example() {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const cadastrarUsuario = await fetch('http://localhost:8080/salvar', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ nome, email, senha }),
+            });
+
+            if (cadastrarUsuario.ok) {
+                alert('Usuário cadastrado com sucesso!');
+                // Limpar os campos do formulário após o cadastro
+                setNome('');
+                setEmail('');
+                setSenha('');
+            } else {
+                alert('Ocorreu um erro ao cadastrar o usuário.');
+            }
+        } catch (error) {
+            console.error('Erro ao enviar requisição:', error);
+            alert('Ocorreu um erro ao cadastrar o usuário.');
+        }
+    };
+
+    return (
+        <>
+            {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        */}
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    />
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Cadastre-se
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST">
+                        <div>
+                            <label htmlFor="nome" className="block text-sm font-medium leading-6 text-gray-900">
+                                Nome
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="nome"
+                                    name="nome"
+                                    value={nome}
+                                    onChange={(e) => setNome(e.target.value)}
+                                    type="text"
+                                    autoComplete="nome"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Senha
+                                </label>
+                                {/* <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Esqueceu a senha?
+                                    </a>
+                                </div> */}
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    value={senha}
+                                    onChange={(e) => setSenha(e.target.value)}
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        {/* <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Digite sua senha novamente
+                                </label>
+                                {/* <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Esqueceu a senha?
+                                    </a>
+                                </div> 
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>*/}
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Já é cadastrado?{' '}
+                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Faça login na sua conta RemindMe
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
