e8e1e3ca182329f780f7da9e78e03d35830b21a4
marialluiza <malululicha@gmail.com>
2024-02-28 18:21:16 -0300

    Rota tela de login e correção AuthUser

--- client/src/pages/user-login/index.js
+++ client/src/pages/user-login/index.js
@@ -1,10 +1,8 @@
-import Form from "../register-user/Form";
 
 export default function UserLogin() {
     return(
         <div>
             <h1>Página de Login</h1>
-            <Form />
         </div>
     )
 }
